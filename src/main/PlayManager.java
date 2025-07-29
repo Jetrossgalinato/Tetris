b35c71b0fc10cfb0e3656d9ff93ebae20c55b672
Jetross <jetrossneri07@gmail.com>
2025-07-29 16:20:28 +0800

    completed all mino

--- src/main/PlayManager.java
+++ src/main/PlayManager.java
@@ -5,10 +5,17 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.Random;
 
 import mino.Block;
 import mino.Mino;
+import mino.Mino_Bar;
 import mino.Mino_L1;
+import mino.Mino_L2;
+import mino.Mino_Square;
+import mino.Mino_T;
+import mino.Mino_Z1;
+import mino.Mino_Z2;
 
 public class PlayManager {
 	
@@ -41,8 +48,26 @@ public class PlayManager {
 		MINO_START_Y = top_y + Block.SIZE;
 		
 		//set the starting mino
-		currentMino = new Mino_L1();
+		currentMino = pickMino();
 		currentMino.setXY(MINO_START_X, MINO_START_Y);
+	}
+	
+	private Mino pickMino() {
+		
+		//Pick a random mino
+		Mino mino = null;
+		int i = new Random().nextInt(7);
+		
+		switch(i) {
+		case 0: mino = new Mino_L1();break;
+		case 1: mino = new Mino_L2();break;
+		case 2: mino = new Mino_Square();break;
+		case 3: mino = new Mino_Bar();break;
+		case 4: mino = new Mino_T();break;
+		case 5: mino = new Mino_Z1();break;
+		case 6: mino = new Mino_Z2();break;
+		}
+		return mino;
 	}
 	public void update() {
 		
--- src/mino/Mino_Bar.java
+++ src/mino/Mino_Bar.java
@@ -14,7 +14,7 @@ public class Mino_Bar extends Mino {
 		// * * * *
 		// 
 		b[0].x = x;
-		b[0].x = y;
+		b[0].y = y;
 		b[1].x = b[0].x - Block.SIZE;
 		b[1].y = b[0].y;
 		b[2].x = b[0].x + Block.SIZE;
@@ -35,11 +35,33 @@ public class Mino_Bar extends Mino {
 		tempB[2].x = b[0].x + Block.SIZE;
 		tempB[2].y = b[0].y;
 		tempB[3].x = b[0].x + Block.SIZE*2;
-		tempB[3].x = b[0].y;
+		tempB[3].y = b[0].y;
 		
 		updateXY(1);
 	}
-	public void getDirection2() {}
-	public void getDirection3() {}
-	public void getDirection4() {}
+	public void getDirection2() {
+		
+		// *
+		// *
+		// * 
+		// *
+		tempB[0].x = b[0].x;
+		tempB[0].y = b[0].y;
+		tempB[1].x = b[0].x;
+		tempB[1].y = b[0].y - Block.SIZE;
+		tempB[2].x = b[0].x;
+		tempB[2].y = b[0].y + Block.SIZE;
+		tempB[3].x = b[0].x;
+		tempB[3].y = b[0].y + Block.SIZE*2;
+		
+		updateXY(2);
+	}
+	public void getDirection3() {
+		
+		getDirection1();
+	}
+	public void getDirection4() {
+		
+		getDirection2();
+	}
 }
