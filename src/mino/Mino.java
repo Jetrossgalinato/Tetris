6a302741788ccaaf9323bbf2e491aac814f6b3f2
Jetross <jetrossneri07@gmail.com>
2025-07-29 16:57:43 +0800

    added bottom collision

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -13,6 +13,7 @@ public class Mino {
 	int autoDropCounter = 0;
 	public int direction = 1; //There are 4 directions
 	boolean leftCollision, rightCollision, bottomCollision;
+	public boolean active = true;
 	
 	public void create(Color c) {
 		b[0] = new Block(c);
@@ -156,16 +157,23 @@ public class Mino {
 			KeyHandler.rightPressed = false;
 		}
 		
-		autoDropCounter++;
-		if(autoDropCounter == PlayManager.dropInterval) {
-			
-			//the mino goes down
-			b[0].y += Block.SIZE;
-			b[1].y += Block.SIZE;
-			b[2].y += Block.SIZE;
-			b[3].y += Block.SIZE;
-			autoDropCounter = 0;
+		if(bottomCollision) {
+			active = false;
 		}
+		else {
+			
+			autoDropCounter++;
+			if(autoDropCounter == PlayManager.dropInterval) {
+				
+				//the mino goes down
+				b[0].y += Block.SIZE;
+				b[1].y += Block.SIZE;
+				b[2].y += Block.SIZE;
+				b[3].y += Block.SIZE;
+				autoDropCounter = 0;
+			}
+		}
+		
 	}
 	public void draw(Graphics2D g2) {
 		
